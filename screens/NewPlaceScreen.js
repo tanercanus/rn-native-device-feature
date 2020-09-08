ddbb3000c9c84b4d2cc4402d683e0d557c2fc3ca
tanercanus <tanercanus@yahoo.com>
2020-09-08 22:24:45 +0200

    - Adding camera feature

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -4,10 +4,12 @@ import { useDispatch } from 'react-redux';
 
 import Colors from '../constants/Colors';
 import * as placesActions from '../store/places-actions';
+import ImagePicker from '../components/ImagePicker';
 
 const NewPlaceScreen = props => {
 
   const [titleValue, setTitleValue] = useState('');
+  const [selectedImage, setSelectedImage] = useState();
 
   const dispatch = useDispatch();
 
@@ -15,8 +17,12 @@ const NewPlaceScreen = props => {
     setTitleValue(text);
   };
 
+  const imageTakenHandler = imagePath => {
+    setSelectedImage(imagePath);
+  };
+
   const savePlaceHandler = () => {
-    dispatch(placesActions.addPlace(titleValue));
+    dispatch(placesActions.addPlace(titleValue, selectedImage));
     props.navigation.goBack();
   };
 
@@ -29,6 +35,7 @@ const NewPlaceScreen = props => {
           onChangeText={titleChangeHandler}
           value={titleValue}
         />
+        <ImagePicker onImageTake={imageTakenHandler} />
         <Button
           title="Save Place"
           color={Colors.primary}
